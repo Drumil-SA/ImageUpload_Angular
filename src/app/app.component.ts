074a7aed417044823fdc32548fb1d0593612795b
Drumil-SA <drumil.shah@solutionanalysts.com>
2019-07-10 10:35:26 +0530

    image uploading 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  public imagePath;
+  imgURL: any;
+  public message: string;
+  url = '';
+ 
+  preview(files) {
+    if (files.length === 0)
+      return;
+ 
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+ 
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]); 
+    reader.onload = (_event) => { 
+      this.imgURL = reader.result; 
+    }
+  }
+
+    onSelectFile(event) {
+      if (event.target.files && event.target.files[0]) {
+        var reader = new FileReader();
+  
+        reader.readAsDataURL(event.target.files[0]);
+  
+        reader.onload = (event:any) => {
+          this.url = event.target.result;
+        }
+      }
+    }
+}
